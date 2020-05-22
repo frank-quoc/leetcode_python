62bed41599b3f2ef712e59ba6a05e688c60f3f36
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-05-21 19:26:54 -0700

    Create answer.py

---
+++ Practice-Problems/two-sum/answer.py
@@ -0,0 +1,18 @@
+# My solution:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                    
+# Someone else's more efficient solution:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        computed_sum = {}
+        for i in range(0, len(nums)):
+            required = target - nums[i]
+            if required in computed_sum: 
+                return [computed_sum[required], i]
+            computed_sum[nums[i]] = i
+        return []
