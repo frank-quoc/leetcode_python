1d0bb411b926ea896e06d58fd6187d8a6d056aea
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-11-08 16:35:34 -0800

    Update answer.py

--- Practice-Problems/reverse-integer/answer.py
+++ Practice-Problems/reverse-integer/answer.py
@@ -1,12 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        x_str = str(x)
-        x_str = x_str[::-1]
+        x_rev = str(x)
+        x_rev = x_str[::-1]
         
-        if x_str[-1] == '-':
-            x_str = x_str[-1] + x_str[:-1]
+        if x_rev[-1] == '-':
+            x_rev = x_rev[-1] + x_rev[:-1]
             
-        x_str = int(x_str)
-        if x_str not in range(-2**31, 2**31):
+        x_rev = int(x_revstr)
+        if x_rev not in range(-2**31, 2**31):
             return 0
-        return x_str
+        return x_rev
